607d7aa433a3245fbd9c0b6aa3e4de5cf60197dc
AHDdev <din5425@gmail.com>
2018-05-05 03:45:30 +0300

    Added some files and folders
    Created main folders of project;
    Added stylesheets;
    Added server configuration file;
    Added login page.

---
+++ FMS/login.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <title>Система паспортного учета ФМС</title>
+    <link rel="shortcut icon" href="images/icon_ufms.png" type="image/png">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body class="container">
+  <div id="login_form_container" class="container">
+    <div id="login_form_header" class="d-flex">
+    <img id="login_form_logo" src="images/ufms.png" alt="Error" class="order-2 p-2">
+    <p id="login_form_logo_text" class="align-self-center order-3 p-2 text-center">ПАСПОРТНЫЙ УЧЕТ ФЕДЕРАЛЬНАЯ МИГРАЦИОННАЯ СЛУЖБА</p>
+    </div>
+    <div id="login_form_fields" class="col-6 mx-auto">
+    <form>
+  <div class="form-group">
+    <input type="login" class="form-control" id="login_input" placeholder="Логин...">
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control" id="password_input" placeholder="Пароль...">
+  </div>
+  <button type="submit" class="btn btn-outline-dark col-md-6 offset-md-3">Войти</button>
+</form>
+    </div>
+  </div>
+  </body>
+</html>
