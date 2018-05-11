d6b238b47dd816fa61a88432213e45f1cddacb73
AHDdev <din5425@gmail.com>
2018-05-11 19:16:57 +0300

    Some fixes in adaptability of pages header

--- FMS/includes/header_container.php
+++ FMS/includes/header_container.php
@@ -1,6 +1,10 @@
 ﻿<header id="header_container" class="d-flex border-bottom border-dark">
-    <img id="header_logo" src="images/header_ufms.png" alt="Error" class="order-1 p-2">
-    <p id="header_text" class="align-self-center order-2 p-2 text-center col-5">ПАСПОРТНЫЙ УЧЕТ ФЕДЕРАЛЬНАЯ МИГРАЦИОННАЯ СЛУЖБА</p>
-    <p id="header_username" class="align-self-center order-3 p-2 text-center col-3">Здравствуйте, Very long username</p>
-    <a id="header_logout" href="#" class="align-self-center order-4 text-center col-1"><img src="images/header_logout.png" alt="error" class="mb-3"></a>
+<div class="col-md-9 d-flex">
+    <img id="header_logo" src="images/header_ufms.png" alt="Error" class="p-2">
+    <p id="header_text" class="align-self-center p-2 text-center">ПАСПОРТНЫЙ УЧЕТ ФЕДЕРАЛЬНАЯ МИГРАЦИОННАЯ СЛУЖБА</p>
+    </div>
+    <div class="col-md-3 d-flex">
+    <p id="header_username" class="align-self-center order-3 p-2 text-right col-10">Здравствуйте, Very long username</p>
+    <a id="header_logout" href="#" class="align-self-center order-4 text-center col-1"><img src="images/header_logout.png" alt="err" class="mb-3"></a>
+    </div>
     </header>
