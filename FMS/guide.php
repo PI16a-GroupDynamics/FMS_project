7da13a05fb166c1d6677fca84ba89773cfe09e7f
SecondSony <sekrethieu@yahoo.com>
2018-05-24 00:43:33 +0300

    Updated guide.php
    Added guide about using this site
    
    Co-Authored-By: sb9itoslab <sb9itoslab@users.noreply.github.com>

--- FMS/guide.php
+++ FMS/guide.php
@@ -24,11 +24,13 @@ if ($_SESSION['loged_user']==0)//проверка на авторизацию
    require_once("includes/left_menu_container.php");
    ?>
    <div id="guide_content" class="col-md-8 rounded border border-dark mb-3 p-5">
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum deserunt, nobis recusandae nostrum explicabo, quaerat, praesentium sapiente placeat unde vel architecto! Consequatur eos fuga fugit nisi, mollitia sunt quod adipisci?
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum deserunt, nobis recusandae nostrum explicabo, quaerat, praesentium sapiente placeat unde vel architecto! Consequatur eos fuga fugit nisi, mollitia sunt quod adipisci?
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum deserunt, nobis recusandae nostrum explicabo, quaerat, praesentium sapiente placeat unde vel architecto! Consequatur eos fuga fugit nisi, mollitia sunt quod adipisci?
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum deserunt, nobis recusandae nostrum explicabo, quaerat, praesentium sapiente placeat unde vel architecto! Consequatur eos fuga fugit nisi, mollitia sunt quod adipisci?
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum deserunt, nobis recusandae nostrum explicabo, quaerat, praesentium sapiente placeat unde vel architecto! Consequatur eos fuga fugit nisi, mollitia sunt quod adipisci?
+       <b><h4 align="center">Справка</h4></b><br>
+    <p><b>1.  Добавление.</b> Что бы добавить новую запись о пользователе нужно: перейти на страницу добавления, нажав на кнопку добавить.</b> После необходимо заполнить все поля и нажать кнопку сохранить.</p>
+    <p><b>2.  Сортировка.</b> Что бы осуществить сортировку данных о пользователей нужно выбрать нужный параметр и вписать нужное вам значение, после нужно нажать на кнопку сортировка.</p>
+    <p><b>3.  Поиск.</b> Что бы осуществить поиск в данных о пользователей нужно вписать нужное вам значение, после нужно нажать на кнопку поиск.</p>
+    <p><b>4.  Подробная информация о пользователе.</b> Для получения подробной информации о пользователе необходимо нажать на строку пользователя в таблице на главной странице.</p>
+    <p><b>5.  Редактирование.</b> Что бы отредактировать данные о пользователе необходимо перейти на страницу с подробной информацией о пользователе и нажать на кнопку редактировать, после чего редактируете информацию и нажимаете на кнопку сохранить.</p>
+
 
    </div>
     
