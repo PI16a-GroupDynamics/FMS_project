636e1b125830d9e6e1cc55eb39daa6de8de8d89b
GruzanSergey <36263843+hizti@users.noreply.github.com>
2018-05-30 19:34:41 +0300

    Update edit.php

--- FMS/edit.php
+++ FMS/edit.php
@@ -106,7 +106,7 @@ function MkHouseValues(index, name){
  </div>
    <div class="form-group d-flex mt-3">
  <label for="adress" class="col-md-3 mt-2">Место рождения:</label>
-<input type="text" minlength="3" required name="birthplace" value=<?php echo "'".$row[14]."'"; ?> class="form-control col-md-9 border-dark" id="add_adress">
+<input type="text" minlength="3" pattern="[А-Я]{1}[а-я]+$" required name="birthplace" value=<?php echo "'".$row[14]."'"; ?> class="form-control col-md-9 border-dark" id="add_adress">
  </div>
  <div class="input-group d-flex mt-3" >
 <p class="col-3 mb-3">Пол:</p>
@@ -204,7 +204,7 @@ function MkHouseValues(index, name){
  </div>
  <div class="form-group d-flex mt-3">
  <label for="add_nationality" class="col-md-2 mt-2">Серия:</label>
-<input type="text" name="serial" required pattern="[А-Я]{2,4}" required minlength="2" maxlength="4" value=<?php echo "'".$row[10]."'"; ?> class="form-control col-md-2 border-dark" id="add_nationality">
+<input type="text" name="serial" required pattern="[0-9]{3} [0-9]{3}" required minlength="2" maxlength="4" value=<?php echo "'".$row[10]."'"; ?> class="form-control col-md-2 border-dark" id="add_nationality">
 <label for="add_nationality" class="col-md-2 mt-2 offset-md-1">Номер:</label>
 
 <input type="text" name="number" pattern="[0-9]{6}" value=<?php echo "'".$row[11]."'"; ?> required  class="form-control col-md-3 border-dark" id="add_nationality">
